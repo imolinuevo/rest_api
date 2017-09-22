af30e7a583e35aa7f2ed52b67b1b7c8436342a7b
Iñigo Molinuevo Parron <imol@imp.local>
2017-09-22 19:27:49 +0200

    Base app clean

--- django/prueba/controllers/use_case_example.py
+++ django/prueba/controllers/use_case_example.py
@@ -5,7 +5,7 @@ from prueba.views import require_post_params, cors_response, execute_query
 
 @require_http_methods(["GET", "OPTIONS"])
 def test_get(request):
-    ret = execute_query('| inputlookup "traffic_violations.csv" | head 10')
+    ret = execute_query('| inputlookup geo_attr_countries.csv')
     context = {"Test": "Example get", "ret": ret}
     return cors_response(context)
 
@@ -13,5 +13,6 @@ def test_get(request):
 @require_http_methods(["POST", "OPTIONS"])
 @require_post_params(params=['email'])
 def test_post(request):
-    context = {"Test": "Example post"}
+    ret = execute_query('| inputlookup geo_attr_countries.csv')
+    context = {"Test": "Example post", "ret": ret}
     return cors_response(context)
--- django/prueba/urls.py
+++ django/prueba/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from splunkdj.utility.views import render_template as render
 
 urlpatterns = patterns('',
-    url(r'^home/$', 'prueba.custom_views.home', name='home'),
+    url(r'^home/$', 'prueba.views.home', name='home'),
     url(r'^auth-jwt/$', 'prueba.views.auth_jwt', name='auth_jwt'),
-    url(r'^test-get/$', 'prueba.custom_views.use_case1.test_get', name='test_get'),
-    url(r'^test-post/$', 'prueba.custom_views.use_case1.test_post', name='test_post')
+    url(r'^test-get/$', 'prueba.controllers.use_case_example.test_get', name='test_get'),
+    url(r'^test-post/$', 'prueba.controllers.use_case_example.test_post', name='test_post')
 )
